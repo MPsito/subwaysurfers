f40a2662d6fb71a4435344e1e1ce75a39eb1f353
MPsito <palomaresmauricio985@gmail.com>
2026-02-15 03:38:15 -0600

    casa1

--- Assets/scripts/character.cs
+++ Assets/scripts/character.cs
@@ -1,106 +1,235 @@
 using UnityEngine;
-using DG.Tweening;
+
+using DG. Tweening;
+
 using System.Collections;
 
-public class character : MonoBehaviour
-{
- private Rigidbody characterRigidBody;
- [SerializeField]
- private characterData characterData;
- [SerializeField]
- private Animator characterAnimator;
- [SerializeField]
- private Transform characterStartpivot;
- [SerializeField]
- private float jumpForce = 5f;
- [SerializeField]
- private float distanceTomove = 2f;
- [SerializeField]
- private float moveDuration = 0.2f;
- private bool isGrounded = true;
- private bool isMoving = false;
- private bool isRolling = false;
- private bool isActive = false;
- private void Awake()
- {
-    characterRigidBody = GetComponent<Rigidbody>();
- }
- public void StartGame()
-    {
-        isRolling = false;
-        isMoving = false;
-        isActive = true;
-        characterAnimator.Play(characterData.runAnimationName, 0, 0f);
-        transform.position =characterStartpivot.position;
-    }
-    public void Lose()
-    {
-        isActive = false;
-        StopAllCoroutines();
-        characterAnimator.Play(characterData.loseAnimationName, 0, 0f);
-    }
-    public void Jump()
-    {
-        if (isActive)return;
-        if (isGrounded)
-        {
-            characterAnimator.Play(characterData.jumpAnimationName, 0, 0f);
-            characterRigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isGrounded = false;
-        }     
-          
-    }
-    public void MoveDown()
-        {
-            if (isActive || isRolling)return;
-            if (!isGrounded)
-           {
-            characterRigidBody.AddForce(Vector3.down * jumpForce * 2, ForceMode.Impulse);
-           }
-           characterAnimator.Play(characterData.rollAnimationName, 0, 0f);
-           isRolling = true;
-           StartCoroutine(ResetRoll());
-        }
-     
-    public void Moveleft()
-    {
-        if (transform.position.x <= distanceTomove) return;
-        Move(Vector3.left);
-    }
-    public void MoveRight()
-    {
-           if (transform.position.x >= distanceTomove) return;
-           Move(Vector3.right);
-    }
-    private void Move(Vector3 direction)
-    {
-        if (isMoving || !isActive) return;
-        characterAnimator.Play(characterData.moveAnimationName, 0, 0.2f);
-        isMoving = true;
-        Vector3 targetPosition = transform.position + direction * distanceTomove;
-        transform.DOMove(targetPosition, moveDuration).SetEase(Ease.OutQuad). OnComplete(() =>
-        {
-            isMoving = false;
-        });
-    }
-     private IEnumerator ResetRoll()
-    {
-        yield return new WaitForSeconds(characterAnimator.GetCurrentAnimatorStateInfo(0).length);
-        isRolling = false;
-    }
-    public void  OnCollisionEnter(Collision collision) 
-    {
-        if (isActive && collision.gameObject.CompareTag("Ground"))
-        {
-            if (!isRolling)
-            {
-                characterAnimator.Play(characterData.runAnimationName, 0, 0f);
-            }
-            isGrounded = true;
-            characterAnimator.Play(characterData.runAnimationName, 0, 0f);
-            isGrounded = true;
-        }
-    }
-   
+using UnityEngine.Events;
  
+public class Character : MonoBehaviour
+
+{
+
+    private Rigidbody characterRigidbody;
+
+    [SerializeField]
+
+    private characterData characterData;
+
+    [SerializeField]
+
+    private Animator characterAnimator;
+
+    [SerializeField]
+
+    private float jumpForce = 5f;
+
+    [SerializeField]
+
+    private float distanceToMove = 2f;
+
+    [SerializeField]
+
+    private float moveDuration = 0.2f;
+
+    [SerializeField]
+
+    private Transform ChararacterStartPivot;
+
+    [SerializeField]
+
+    private UnityEvent onJump;
+
+    [SerializeField]
+
+    private UnityEvent onMoveToSide;
+
+    [SerializeField]
+
+    private UnityEvent onRoll;
+
+    [SerializeField]
+
+    private UnityEvent onLose;
+
+    private bool isGrounded= true;
+
+    private bool isMoving = false;
+
+    private bool isRolling = false;
+
+    private bool isActive = false;
+
+    private void Awake()
+
+    {
+
+        characterRigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    public void StartGame()
+
+    {
+
+        isRolling = false;
+
+        isMoving = false;
+
+        isActive = true;
+
+        characterAnimator.Play(characterData.runAnimationName, 0, 0f);
+
+        transform.position= ChararacterStartPivot.position;
+
+    }
+
+    private void Start()
+
+    {
+
+        isActive = true;
+
+        characterAnimator.Play(characterData.runAnimationName, 0, 0f);
+
+        characterRigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    public void Lose()
+
+    {
+
+        isActive = false;
+
+        StopAllCoroutines();
+
+        characterAnimator.Play(characterData.loseAnimationName, 0, 0f);
+
+    }
+
+    public void Jump()
+
+    {
+
+        if (!isActive) return;
+
+        if (isGrounded)
+
+        {
+
+            onJump?.Invoke();
+
+            characterAnimator.Play(characterData.jumpAnimationName, 0, 0f);
+
+            characterRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            isGrounded = false;
+
+        }
+
+    }
+
+    public void MoveDown()
+
+    {
+
+        if (!isActive || isRolling) return;
+
+        if (!isGrounded)
+
+        {
+
+            characterRigidbody.AddForce(Vector3.down * jumpForce * 2, ForceMode.Impulse);
+
+        }
+
+        characterAnimator.Play(characterData.rollAnimationName, 0, 0f);
+
+        onRoll?.Invoke();
+
+        isRolling = true;
+
+        StartCoroutine(ResetRoll());
+
+    }
+
+    public void MoveLeft()
+
+    {
+
+        if (transform.position.x <= -distanceToMove) return;
+
+        Move(Vector3.left);
+
+    }
+
+    public void MoverRight()
+
+    {
+
+        if (transform.position.x >= distanceToMove) return;
+
+        Move(Vector3.right);
+
+    }
+
+    private void Move(Vector3 direction)
+
+    {
+
+        if (isMoving || !isActive) return;
+
+        onMoveToSide?.Invoke();
+
+        characterAnimator.Play(characterData.moveAnimationName, 0, 0f);
+
+        isMoving= true;
+
+        Vector3 targetPosition = transform.position + direction * distanceToMove;
+ 
+        transform.DOMove(targetPosition, moveDuration).SetEase(Ease.OutQuad).OnComplete(()=>
+
+        {
+
+           isMoving = false; 
+
+        });
+
+    }
+
+    private IEnumerator ResetRoll()
+
+    {
+
+        yield return new WaitForSeconds(characterAnimator.GetCurrentAnimatorStateInfo(0).length);
+
+        isRolling = false;
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+
+    {
+
+        if (isActive && collision.gameObject.CompareTag("Ground"))
+
+        {
+
+            if (!isRolling)
+
+            {
+
+                characterAnimator.Play(characterData.runAnimationName, 0, 0f);
+
+            }
+
+            isGrounded = true;
+
+        }
+
+    }
+
 }
+
+ 
