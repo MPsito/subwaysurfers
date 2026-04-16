89880fade35f6a4b50d6e7e37fd6bb34611e3a6b
MPsito <palomaresmauricio985@gmail.com>
2026-04-16 17:27:33 -0600

    pausemenu

---
+++ Assets/scripts/PauseManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+[SerializeField]
+private string pauseMenuOpenAnimation = "show";
+[SerializeField]
+private string pauseMenuCloseAnimation = "Hide";
+[SerializeField]
+private Animator pauseMenuAnimator;
+private bool isPaused = false;
+public void PauseGame()
+    {
+        if (isPaused) return;
+        Time.timeScale = 0f;
+        pauseMenuAnimator.Play(pauseMenuOpenAnimation, 0, 0f);
+    }
+    public void ResumeGame()
+    {
+        if (!isPaused) return;
+        Time.timeScale = 1f;
+        isPaused = false;
+        pauseMenuAnimator.Play(pauseMenuCloseAnimation, 0, 0f);
+    }
+}
